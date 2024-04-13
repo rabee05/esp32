de2214b578948dba7383f83f76713012971783df
rabee05 <rabee05@hotmail.com>
2024-04-13 00:10:45 -0400

    wifi service only

--- src/main.cpp
+++ src/main.cpp
@@ -5,42 +5,24 @@
 #include <BLE2902.h>
 #include <WiFi.h>
 
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
 #define SERVICE_UUID_1 "8cd0f8e8-ccbe-4b89-b41e-e2951da0e8ae"
-#define CHARACTERISTIC_UUID_1_ALL "2b4f4ecd-7a3a-4f89-af91-6f4d4ce2e6db"
-#define CHARACTERISTIC_UUID_1_R "beda398f-b495-4c1f-b06a-fa086e24cdb1"
-#define CHARACTERISTIC_UUID_1_N "98106d21-0955-4a40-882b-e64017b914cf"
-#define CHARACTERISTIC_UUID_1_W_WIFI "0bc1cb7d-3ff4-486d-b60d-78c084fa73ae"
-#define CHARACTERISTIC_UUID_1_WIFI_CONNECT "0ecbf7ca-60c2-11ee-8c99-0242ac120002"
+#define CHARACTERISTIC_UUID "0bc1cb7d-3ff4-486d-b60d-78c084fa73ae"
 
 BLEServer *pServer = NULL;
-BLECharacteristic *pCharacteristic_1_1 = NULL;
-BLECharacteristic *pCharacteristic_1_2 = NULL;
-BLECharacteristic *pCharacteristic_1_3 = NULL;
-BLECharacteristic *pCharacteristic_1_4 = NULL;
-
-BLE2902 *descriptor_1_1 = NULL;
-BLE2902 *descriptor_1_2 = NULL;
-BLE2902 *descriptor_1_3 = NULL;
-BLE2902 *descriptor_1_4 = NULL;
+BLECharacteristic *pCharacteristic_wifi = NULL;
+BLE2902 *descriptor_wifi = NULL;
 
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-uint32_t value = 0;
-bool streamFlag = false;
-bool isRead = false;
-uint32_t x = 0;
-uint8_t batteryLevel = 57;
-
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
 
 class MyServerCallbacks : public BLEServerCallbacks
 {
   void onConnect(BLEServer *pServer)
   {
     deviceConnected = true;
-    isRead = true;
-    // BatteryLevelDescriptor->setNotifications(true);
   };
 
   void onDisconnect(BLEServer *pServer)
@@ -51,33 +33,34 @@ class MyServerCallbacks : public BLEServerCallbacks
 
 class MyCallbacks : public BLECharacteristicCallbacks
 {
+
   void onWrite(BLECharacteristic *mypCharacteristic)
   {
     std::string rxValue = mypCharacteristic->getValue();
+
     std::string data = "";
     char command;
-    if (!rxValue.empty() && rxValue.length() > 1)
+    if (!rxValue.empty() && rxValue.length() > 1 && rxValue.find(":"))
     {
-      data = rxValue.substr(1);
+      command = rxValue[0];
+      data = rxValue.substr(2);
+      // Serial.println(data.c_str());
     }
     else
     {
       command = rxValue[0];
+      data = "";
+      // Serial.println(command);
     }
 
     if (!rxValue.empty())
     {
       switch (command)
       {
-      case 'a':
-        descriptor_1_1->setNotifications(true); // not sure if I needed it
-        streamFlag = true;
-        break;
-
       case 's':
       {
         String availableNetworks = scanNetworks();
-        // Serial.println(availableNetworks);
+        Serial.println(availableNetworks);
         int beginIndex = 0;
         int endIndex = 0;
 
@@ -94,8 +77,8 @@ class MyCallbacks : public BLECharacteristicCallbacks
 
           if (deviceConnected)
           {
-            pCharacteristic_1_3->setValue(chunkData);
-            pCharacteristic_1_3->notify();
+            pCharacteristic_wifi->setValue(chunkData);
+            pCharacteristic_wifi->notify();
             delay(100);
           }
 
@@ -106,23 +89,25 @@ class MyCallbacks : public BLECharacteristicCallbacks
 
       case 'c':
       {
-        Serial.println("rxValue.c_str()");
         size_t delimiterPos = data.find(":");
         if (delimiterPos != std::string::npos)
         {
+          Serial.println("Yes, ssid and pass were sent");
           std::string receivedSSID = data.substr(0, delimiterPos);
           std::string receivedPassword = data.substr(delimiterPos + 1);
-          // Serial.println(receivedSSID.c_str());
-          // Serial.println(receivedPassword.c_str());
+          Serial.println(receivedSSID.c_str());
+          Serial.println(receivedPassword.c_str());
           if (connectWiFi(receivedSSID.c_str(), receivedPassword.c_str()))
           {
-            pCharacteristic_1_4->setValue("connected");
-            pCharacteristic_1_4->notify();
+            pCharacteristic_wifi->setValue("connected");
+            pCharacteristic_wifi->notify();
+            Serial.println("connected");
           }
           else
           {
-            pCharacteristic_1_3->setValue("error");
-            pCharacteristic_1_3->notify();
+            pCharacteristic_wifi->setValue("error");
+            pCharacteristic_wifi->notify();
+            Serial.println("connection error");
           }
         }
 
@@ -132,26 +117,30 @@ class MyCallbacks : public BLECharacteristicCallbacks
       case 'e':
         if (toggleWiFi("enable"))
         {
-          pCharacteristic_1_3->setValue("enabled");
-          pCharacteristic_1_3->notify();
+          pCharacteristic_wifi->setValue("enabled");
+          pCharacteristic_wifi->notify();
+          Serial.println("toggled-enabled");
         }
         else
         {
-          pCharacteristic_1_3->setValue("error");
-          pCharacteristic_1_3->notify();
+          pCharacteristic_wifi->setValue("error");
+          pCharacteristic_wifi->notify();
+          Serial.println("toggled-enabled-error");
         }
         break;
 
       case 'd':
         if (toggleWiFi("disable"))
         {
-          pCharacteristic_1_3->setValue("disabled");
-          pCharacteristic_1_3->notify();
+          pCharacteristic_wifi->setValue("disabled");
+          pCharacteristic_wifi->notify();
+          Serial.println("Toggled-disabled");
         }
         else
         {
-          pCharacteristic_1_3->setValue("error");
-          pCharacteristic_1_3->notify();
+          pCharacteristic_wifi->setValue("error");
+          pCharacteristic_wifi->notify();
+          Serial.println("Toggled-disabled-error");
         }
         break;
 
@@ -175,8 +164,9 @@ class MyCallbacks : public BLECharacteristicCallbacks
 
           if (deviceConnected)
           {
-            pCharacteristic_1_3->setValue(chunkData);
-            pCharacteristic_1_3->notify();
+            pCharacteristic_wifi->setValue(chunkData);
+            pCharacteristic_wifi->notify();
+            Serial.println(chunkData);
             delay(100); // A delay to ensure data is sent and processed before the next chunk
           }
 
@@ -352,48 +342,20 @@ void setup()
   // Create the BLE Service
   BLEService *pService_1 = pServer->createService(SERVICE_UUID_1);
 
-  // Create a BLE Characteristic
-  pCharacteristic_1_1 = pService_1->createCharacteristic(
-      CHARACTERISTIC_UUID_1_ALL,
-      BLECharacteristic::PROPERTY_READ |
-          BLECharacteristic::PROPERTY_WRITE |
-          BLECharacteristic::PROPERTY_NOTIFY |
-          BLECharacteristic::PROPERTY_INDICATE);
-
-  pCharacteristic_1_2 = pService_1->createCharacteristic(
-      CHARACTERISTIC_UUID_1_R,
-      BLECharacteristic::PROPERTY_READ);
-
-  pCharacteristic_1_3 = pService_1->createCharacteristic(
-      CHARACTERISTIC_UUID_1_W_WIFI,
+  pCharacteristic_wifi = pService_1->createCharacteristic(
+      CHARACTERISTIC_UUID,
       BLECharacteristic::PROPERTY_WRITE |
           BLECharacteristic::PROPERTY_NOTIFY |
-          BLECharacteristic::PROPERTY_READ);
-
-  pCharacteristic_1_4 = pService_1->createCharacteristic(
-      CHARACTERISTIC_UUID_1_WIFI_CONNECT,
-      BLECharacteristic::PROPERTY_WRITE |
-          BLECharacteristic::PROPERTY_NOTIFY |
-          BLECharacteristic::PROPERTY_READ);
+          BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_INDICATE |
+          BLECharacteristic::PROPERTY_WRITE_NR |
+          BLECharacteristic::PROPERTY_BROADCAST);
 
   // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
   // Create a BLE Descriptor
-
-  descriptor_1_1 = new BLE2902();
-  pCharacteristic_1_1->addDescriptor(descriptor_1_1);
-  pCharacteristic_1_1->setCallbacks(new MyCallbacks());
-
-  descriptor_1_2 = new BLE2902();
-  pCharacteristic_1_1->addDescriptor(descriptor_1_2);
-  pCharacteristic_1_2->setCallbacks(new MyCallbacks());
-
-  descriptor_1_3 = new BLE2902();
-  pCharacteristic_1_3->addDescriptor(descriptor_1_3);
-  pCharacteristic_1_3->setCallbacks(new MyCallbacks());
-
-  descriptor_1_4 = new BLE2902();
-  pCharacteristic_1_4->addDescriptor(descriptor_1_4);
-  pCharacteristic_1_4->setCallbacks(new MyCallbacks());
+  descriptor_wifi = new BLE2902();
+  pCharacteristic_wifi->addDescriptor(descriptor_wifi);
+  pCharacteristic_wifi->setCallbacks(new MyCallbacks());
 
   // Start the service
   pService_1->start();
@@ -411,19 +373,9 @@ void setup()
 void loop()
 {
   // notify changed value
-  if (deviceConnected && streamFlag)
-  {
-    pCharacteristic_1_1->setValue((uint8_t *)&value, 4);
-    pCharacteristic_1_1->notify();
-    Serial.println(value);
-    value++;
-
-    delay(1000); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
-  }
-
   if (deviceConnected)
   {
-    // pCharacteristic_1_2 ->setValue(x);
+    delay(1000); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
   }
 
   // disconnecting
@@ -438,7 +390,6 @@ void loop()
   if (deviceConnected && !oldDeviceConnected)
   {
     // do stuff here on connecting
-
     oldDeviceConnected = deviceConnected;
   }
 }
